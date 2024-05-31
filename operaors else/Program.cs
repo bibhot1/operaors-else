7267476e23fd68bda74f7fdd605368b4be79d5bd
Bibhot1 <bndazbvu.me12@gmail.com>
2024-06-01 07:15:26 +1200

    Add project files.

---
+++ operaors else/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Please specify your age");
+int age = Int32.Parse(Console.ReadLine());
+
+
+if (age > 18)
+{
+    Console.WriteLine("Go party in the club");
+}
+else if(age == 18)
+{
+    Console.WriteLine("you can still party in the school ");
+}
+else
+{
+    Console.WriteLine("Stay at home");
+}
